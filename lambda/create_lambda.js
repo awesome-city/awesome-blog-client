57a1b990250417c88c7f7b54ed2fb3940f133496
taigacat <taigacat@users.noreply.github.com>
2022-11-17 09:05:09 +0900

    Feature/add cloudformation deploy flow (#7)
    add: cloudformation template
    
    Co-authored-by: taigacat <taigacat@users.noreplay.github.com>

--- lambda/create_lambda.js
+++ lambda/create_lambda.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var util = require('util');
+var archiver = require('archiver')
 
 var template = fs.readFileSync(path.join(__dirname, 'index.template.js'), 'utf8');
 
@@ -12,4 +13,15 @@ if (!fs.existsSync(path.join(__dirname, '../dist/lambda'))) {
   fs.mkdirSync(path.join(__dirname, '../dist/lambda'));
 }
 
+var lambda_path = path.join(__dirname, '../dist/lambda/index.js');
 fs.writeFileSync(path.join(__dirname, '../dist/lambda/index.js'), lambda);
+
+(async () => {
+  var zipOutput = fs.createWriteStream(path.join(__dirname, '../dist/lambda/lambda.zip'));
+  var archive = archiver('zip', {
+    zlib: {level: 9}
+  });
+  archive.pipe(zipOutput);
+  archive.glob(lambda_path);
+  await archive.finalize();
+})();
