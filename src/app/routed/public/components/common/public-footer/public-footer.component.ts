09f8e40932f53ccd8c28c2222ce24bbd9e9d0940
taigacat <taigacat@users.noreply.github.com>
2022-11-25 12:19:52 +0900

    #BLOG-10 fix: footer position

--- src/app/routed/public/components/common/public-footer/public-footer.component.ts
+++ src/app/routed/public/components/common/public-footer/public-footer.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { appSelectors } from '../../../../../store/selectors/app.selector';
+import { fromEvent, Subscription, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-public-footer',
   templateUrl: './public-footer.component.html',
   styleUrls: ['./public-footer.component.scss'],
 })
-export class PublicFooterComponent {
+export class PublicFooterComponent implements OnInit, OnDestroy {
   site$ = this.store.select(appSelectors.getSite);
+  sub$ = new Subscription();
   today = new Date();
+  position: 'sticky' | 'block' = 'block';
+  top?: number;
+
   constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.sub$.add(
+      fromEvent(window, 'resize')
+        .pipe(throttleTime(100))
+        .subscribe((_) => {
+          this.adjustFooterPosition();
+        })
+    );
+    this.adjustFooterPosition();
+  }
+
+  private adjustFooterPosition() {
+    const windowHeight = window.innerHeight;
+    const bodyHeight = document.body.clientHeight;
+
+    if (windowHeight > bodyHeight) {
+      this.position = 'sticky';
+      const elems = document.getElementsByName('footer');
+      if (elems && elems.length > 0) {
+        this.top = windowHeight - elems[0].clientHeight;
+      }
+    } else {
+      this.position = 'block';
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.sub$.unsubscribe();
+  }
 }
