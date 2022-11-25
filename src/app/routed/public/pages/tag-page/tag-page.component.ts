1dce6075ac6328558a637a0b2688bbf73c208df2
taigacat <taigacat@users.noreply.github.com>
2022-11-26 08:48:03 +0900

    #BLOG-13 add: components

--- src/app/routed/public/pages/tag-page/tag-page.component.ts
+++ src/app/routed/public/pages/tag-page/tag-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { ArticleSelector } from '../../store/selectors/articleSelector';
+import { Subscription } from 'rxjs';
+import { State } from '../../../../store/app.state';
+import { ArticleAction } from '../../store/actions/article.action';
 
 @Component({
   selector: 'app-tag-page',
   templateUrl: './tag-page.component.html',
   styleUrls: ['./tag-page.component.scss'],
 })
-export class TagPageComponent {
-  constructor() {}
+export class TagPageComponent implements OnInit, OnDestroy {
+  tagId?: string;
+  articles$ = this.store.pipe(select(ArticleSelector.getArticles));
+  sub$ = new Subscription();
+
+  constructor(private store: Store<State>, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.tagId = this.route.snapshot.params['id'];
+    if (this.tagId) {
+      this.store.dispatch(ArticleAction.loadByTag({ tagId: this.tagId, limit: 10 }));
+    }
+  }
+
+  ngOnDestroy() {
+    this.sub$.unsubscribe();
+  }
 }
--- src/app/routed/public/store/actions/article.action.ts
+++ src/app/routed/public/store/actions/article.action.ts
@@ -6,14 +6,25 @@ import { GetArticleResponse } from '../../../../models/http/article/get-article-
 export const ArticleAction = createActionGroup({
   source: 'Article',
   events: {
+    // 記事
     'load one': props<{ id: string; reload?: boolean }>(),
     'load one success': props<{ result?: GetArticleResponse }>(),
     'load one failure': props<{ error: any }>(),
+
+    // 記事一覧
     load: props<{ limit?: number; reload?: boolean }>(),
     'load success': props<{ result: GetArticlesResponse }>(),
     'load failure': props<{ error: any }>(),
     'load more': props<{ limit?: number }>(),
     'load more success': props<{ result: GetArticlesResponse }>(),
     'load more failure': props<{ error: any }>(),
+
+    // タグ別記事一覧
+    'load by tag': props<{ tagId: string; limit?: number; reload?: boolean }>(),
+    'load by tag success': props<{ result: GetArticlesResponse }>(),
+    'load by tag failure': props<{ error: any }>(),
+    'load more by tag': props<{ tagId: string; limit?: number }>(),
+    'load more by tag success': props<{ result: GetArticlesResponse }>(),
+    'load more by tag failure': props<{ error: any }>(),
   },
 });
