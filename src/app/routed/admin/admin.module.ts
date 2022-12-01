fb4a85d4ef5d96b9ecf417e9b0861d54288c4be2
taigacat <taigacat@users.noreply.github.com>
2022-12-02 08:33:41 +0900

    feat: add header and sidebar for admin

--- src/app/routed/admin/admin.module.ts
+++ src/app/routed/admin/admin.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { SharedModule } from '../../shared/shared.module';
-import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
+import { AdminSidebarComponent } from './components/common/admin-sidebar/admin-sidebar.component';
 import { AdminComponent } from './admin.component';
+import { AdminHeaderComponent } from './components/common/admin-header/admin-header.component';
 
 @NgModule({
-  declarations: [AdminSidebarComponent, AdminComponent],
+  declarations: [AdminSidebarComponent, AdminComponent, AdminHeaderComponent],
   imports: [CommonModule, SharedModule, AdminRoutingModule],
   exports: [],
 })
