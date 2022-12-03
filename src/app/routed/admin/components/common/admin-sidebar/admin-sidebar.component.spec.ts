62dfceea42f54bdac37b17ffbb20f987a5c1a965
taigacat <taigacat@users.noreply.github.com>
2022-12-04 03:24:50 +0900

    test

--- src/app/routed/admin/components/common/admin-sidebar/admin-sidebar.component.spec.ts
+++ src/app/routed/admin/components/common/admin-sidebar/admin-sidebar.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminSidebarComponent } from './admin-sidebar.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('AdminSidebarComponent', () => {
   let component: AdminSidebarComponent;
@@ -9,6 +10,7 @@ describe('AdminSidebarComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AdminSidebarComponent],
+      imports: [TranslateModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AdminSidebarComponent);
